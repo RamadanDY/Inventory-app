2bf1a415a13d477c750b1f4d0e202ccb2f422773
RamDev <daudalaramadan2@gmail.com>
2025-01-11 21:01:58 +0000

    CONNECTION STRING WORKS NOW

--- app/invoices/new/page.tsx
+++ app/invoices/new/page.tsx
@@ -1,14 +1,18 @@
+import { sql } from "drizzle-orm";
+import { db } from "@/db";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
+  const results = await db.execute(sql`SELECT current_database()`);
   return (
     <main className="flex flex-col h-screen gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-semibold">Create Invoices</h1>
       </div>
+      {JSON.stringify(results)}
 
       <form className="grid gap-4 max-w-xs ">
         <div className="label">
